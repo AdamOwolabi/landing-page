5e33b4e0fffa24c468f07562fcc89580142f1acd
Adam Owolabi <113387571+AdamOwolabi@users.noreply.github.com>
2025-07-10 23:51:48 -0400

    removed the test navbar

--- assets/js/main.js
+++ assets/js/main.js
@@ -4,6 +4,11 @@
 	Free for personal and commercial use under the CCA 3.0 license (html5up.net/license)
 */
 
+	function toggleMenu() {
+	const menu = document.querySelector('.menu');
+	menu.classList.toggle('active');
+	}
+
 (function($) {
 
 	var	$window = $(window),
